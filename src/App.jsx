2314c5313d19d9386be9f6a331cc1265b27f49b5
aaronfnp <aaronfnp@gmail.com>
2025-05-20 19:27:03 -0400

    Final Styling
    - Made thursday look flashy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -323,15 +321,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-header">
+        <h1 className="title">
+          <span className="title-thursday">Thursday</span>
+          <span className="title-plus">M+</span>
+          <div className="title-glow"></div>
+        </h1>
       </div>
-      <h1>Thursday M+</h1>
       
       {/* Delete zone that appears when dragging */}
       <div 
@@ -477,10 +473,6 @@ function App() {
           Create Groups
         </button>
       </div>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
