f4a833c9131c501f7d0c700963e2b7a553cdfacb
aaronfnp <aaronfnp@gmail.com>
2025-05-20 18:38:34 -0400

    basic styling
    - basic styling updated to split into containers
    - deployed to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,20 @@ import './App.css'
 function App() {
   const [players, setPlayers] = useState([{name: "Chris", role: "Healer"},{name: "Aaron", role: "DPS"}])
   const [groups, setGroups] = useState([])
-  const roles = ["Healer", "DPS", "Tank"]
-
+  const roles = [
+    {
+      title: "Healer", 
+      img: "https://ih1.redbubble.net/image.331548773.1098/raf,360x360,075,t,fafafa:ca443f4786.u2.jpg"
+    }, 
+    {
+      title: "DPS", 
+      img: "https://ih1.redbubble.net/image.331565552.1599/raf,360x360,075,t,fafafa:ca443f4786.jpg"
+    }, 
+    {
+      title: "Tank", 
+      img: "https://ih1.redbubble.net/image.331545985.1023/raf,360x360,075,t,fafafa:ca443f4786.jpg"
+    }
+  ]
   return (
     <>
       <div>
@@ -26,11 +38,21 @@ function App() {
           <div className='player-input-container'>
             <li>{player.name}</li> 
             {roles.map(role => (
-              <div>
-                <label>{role}</label>
-                <input type="checkbox"></input>
+              <div key={role.title} className="role-option">
+                <input 
+                  type="checkbox" 
+                  id={`${role.title}-checkbox`} 
+                  aria-label={role.title}
+                />
+                <img 
+                  src={role.img} 
+                  alt={role.title} 
+                  title={role.title} 
+                  className="role-icon" 
+                />
               </div>
             ))}
+
           </div>
         ))}
         </div>
